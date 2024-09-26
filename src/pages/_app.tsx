e862f41fac6b1cdba2740ef7c75c9db7f56c982c
Diyana <diana.ivelinova@gmail.com>
2024-09-26 13:44:29 +0300

    to do list

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import localFont from "next/font/local";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const geistSans = localFont({
+  src: "./oswald-latin-400-normal.woff",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={geistSans.className} style={{ fontWeight: "bold" }}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
