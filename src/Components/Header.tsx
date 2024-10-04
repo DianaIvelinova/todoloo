8dc45c57c96d6487a4aa870562a98e9e91d4f601
Diyana <diana.ivelinova@gmail.com>
2024-10-04 18:08:28 +0300

    todoloo many changes made

---
+++ src/Components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+      <>
+        <header className="mb-5">
+            <img src="/logo.png" alt="Logo" className="logo" />
+        </header>
+      </>
+    );
+  }
+  
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,11 @@
+import Taskfield from "../Components/Taskfield";
+import Header from "../Components/Header";
+
 export default function Home() {
   return (
     <div className="wrapper">
-      <header>
-        <img src="/logo.png" alt="Logo" className="logo" />
-      </header>
-      <form className="row g-3">
-        <div className="col-auto" style={{ flex: 1 }}>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Write your task here..."
-          />
-        </div>
-        <div className="col-auto">
-          <button type="submit" className="btn btn-dark mb-3">
-            Add
-          </button>
-        </div>
-      </form>
+      <Header />
+      <Taskfield />
     </div>
   );
 }
