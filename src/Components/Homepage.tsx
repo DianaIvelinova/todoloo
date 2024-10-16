da652489263c4f3bed3d25ca027bafbe37fe8a66
Diyana <diana.ivelinova@gmail.com>
2024-10-16 18:08:54 +0300

    logo fix

--- src/Components/Homepage.tsx
+++ src/Components/Homepage.tsx
@@ -11,14 +11,12 @@ export default function Homepage() {
   return (
       <div className="vh-100">
         <div className="wrapper">
-          <div className="title">
-            <div className="h1">
-              <h1 className="m-0">Todoloo</h1>
-              <div className="home-text"> Welcome to your daily to-do list.</div>
-              <div className="typewriter mb-4">
-                  <p> Do you want to continue? </p>
-              </div>  
-            </div>
+          <div className="h1">
+            <h1 className="m-0">Todoloo</h1>
+            <div className="home-text"> Welcome to your daily to-do list.</div>
+            <div className="typewriter mb-4">
+                <p> Do you want to continue? </p>
+            </div>  
           </div>
           <button className="home-btn" onClick={handleContinue}><span>continue</span></button>
         </div>  
